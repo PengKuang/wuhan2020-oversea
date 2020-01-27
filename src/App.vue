9647c13e7523e3d010843e476f50c4ffd666dca3
PengKuang <kap.kuang@gmail.com>
2020-01-28 00:23:58 +0800

    set up the frameworks

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SubmitInfo msg="Pray for Wuhan" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SubmitInfo from './components/SubmitInfo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SubmitInfo
   }
 }
 </script>
---
+++ src/components/SubmitInfo.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Submit Info About Flight</h1>
+</template>
+
+<script>
+export default {
+    name: 'SubmitInfo',
+}
+</script>
